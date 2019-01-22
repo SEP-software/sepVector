18fbaeb0950f3155458d8f0fe06f329b83d73324
Robert Clapp <bob@roberts-mbp-3.lan>
2019-01-21 17:10:05 -0800

    update submodule

--- lib/regSpace.h
+++ lib/regSpace.h
@@ -24,7 +24,7 @@ class regSpace {
     if (nw < 1)
       throw(SEPException(std::string("nw must be 1 or greater=") +
                          std::to_string(nw)));
-    if (fw + jw * (nw - 1) < n)
+    if (fw + jw * (nw - 1)  >= n)
       throw(SEPException(std::string("fw+jw*(nw-1) <n ") + std::to_string(fw) +
                          std::string("=fw jw=") + std::to_string(jw) +
                          std::string(" nw=") + std::to_string(nw) +
