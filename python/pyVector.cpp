2a121233664c16e47a0cbc75d2fbf7e12dbbd7f6
Robert Clapp <bob@roberts-mbp-3.lan>
2020-02-03 11:12:47 -0800

    new pybind11

--- python/pyVector.cpp
+++ python/pyVector.cpp
@@ -188,6 +188,42 @@ PYBIND11_MODULE(pySepVector, clsVector) {
                                py::format_descriptor<float>::format(), 1,
                                {m.getHyper()->getAxis(1).n}, {sizeof(float)});
       });
+      py::class_<float3DReg, floatHyper, std::shared_ptr<float3DReg>>(
+      clsVector, "float3DReg", py::buffer_protocol())
+      .def(py::init<const int, const int, const int>(),
+           "Initialize giving size")
+      .def(py::init<std::shared_ptr<hypercube>>(), "Initialize with hypercube")
+      .def(py::init<const axis &, const axis &, const axis &>(),
+           "Initialize from an axis")
+      .def("allocate", (void (float3DReg::*)()) & float3DReg::allocate,
+           "Allocate the array")
+      .def("clone",
+           (std::shared_ptr<float3DReg>(float3DReg::*)() const) &
+               float3DReg::clone,
+           "Make a copy of the vector")
+      .def("cloneSpace",
+           (std::shared_ptr<float3DReg>(float3DReg::*)() const) &
+               float3DReg::cloneSpace,
+           "Make a copy of the vector space")
+      .def("window",
+           (std::shared_ptr<float1DReg>(float3DReg::*)(
+               const std::vector<int> &, const std::vector<int> &,
+               const std::vector<int> &) const) &
+               float3DReg::window,
+           "Window a vector")
+
+
+      .def_buffer([](float3DReg &m) -> py::buffer_info {
+        return py::buffer_info(
+            m.getVals(), sizeof(float), py::format_descriptor<float>::format(),
+            3,
+            {m.getHyper()->getAxis(3).n, m.getHyper()->getAxis(2).n,
+             m.getHyper()->getAxis(1).n},
+            {sizeof(float) * m.getHyper()->getAxis(1).n *
+                 m.getHyper()->getAxis(2).n,
+             sizeof(float) * m.getHyper()->getAxis(1).n, sizeof(float)});
+      });
+
   py::class_<float2DReg, floatHyper, std::shared_ptr<float2DReg>>(
       clsVector, "float2DReg", py::buffer_protocol())
       .def(py::init<const int, const int>(), "Initialize giving size")
@@ -237,41 +273,7 @@ PYBIND11_MODULE(pySepVector, clsVector) {
             {sizeof(float) * m.getHyper()->getAxis(1).n, sizeof(float)});
       });
 
-  py::class_<float3DReg, floatHyper, std::shared_ptr<float3DReg>>(
-      clsVector, "float3DReg", py::buffer_protocol())
-      .def(py::init<const int, const int, const int>(),
-           "Initialize giving size")
-      .def(py::init<std::shared_ptr<hypercube>>(), "Initialize with hypercube")
-      .def(py::init<const axis &, const axis &, const axis &>(),
-           "Initialize from an axis")
-      .def("allocate", (void (float3DReg::*)()) & float3DReg::allocate,
-           "Allocate the array")
-      .def("clone",
-           (std::shared_ptr<float3DReg>(float3DReg::*)() const) &
-               float3DReg::clone,
-           "Make a copy of the vector")
-      .def("cloneSpace",
-           (std::shared_ptr<float3DReg>(float3DReg::*)() const) &
-               float3DReg::cloneSpace,
-           "Make a copy of the vector space")
-      .def("window",
-           (std::shared_ptr<float1DReg>(float3DReg::*)(
-               const std::vector<int> &, const std::vector<int> &,
-               const std::vector<int> &) const) &
-               float3DReg::window,
-           "Window a vector")
-
-      .def_buffer([](float3DReg &m) -> py::buffer_info {
-        return py::buffer_info(
-            m.getVals(), sizeof(float), py::format_descriptor<float>::format(),
-            3,
-            {m.getHyper()->getAxis(3).n, m.getHyper()->getAxis(2).n,
-             m.getHyper()->getAxis(1).n},
-            {sizeof(float) * m.getHyper()->getAxis(1).n *
-                 m.getHyper()->getAxis(2).n,
-             sizeof(float) * m.getHyper()->getAxis(1).n, sizeof(float)});
-      });
-
+  
   py::class_<float4DReg, floatHyper, std::shared_ptr<float4DReg>>(
       clsVector, "float4DReg", py::buffer_protocol())
       .def(py::init<const int, const int, const int, const int>(),
